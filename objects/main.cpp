f56e2a188e9a5cf55d9b718dadd33fa4a4cfd3a8
phansen73 <phansen@nd.edu>
2014-03-26 11:59:30 -0400

    Forgot to add main.cpp

---
+++ objects/main.cpp
@@ -0,0 +1,21 @@
+/*
+main.cpp
+Patrick Hansen
+3/27/14
+purp: for testing the objectes
+*/
+
+#include "Disc.h"
+#include "Hole.h"
+#include "Player.h"
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	Disc disc;
+	Player player = Player("Jimmy Mickle", 3, 5);
+	Hole hole = Hole(150, 50, 5);
+	disc.letFly(14, 12);
+	disc.discPrint();
+}
